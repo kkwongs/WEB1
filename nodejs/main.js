6e39503b56cae42dcd2fadf9753c24572cce724c
kkwongs <kihyun5741@gmail.com>
2021-02-23 18:01:43 +0900

    App - 파일 생성 및 리다이렉션

--- nodejs/main.js
+++ nodejs/main.js
@@ -79,16 +79,18 @@ var app = http.createServer(function (request, response) {
     })
   } else if (pathname === '/create_process') {
     var body = '';
-    request.on('data', function(data) {
+    request.on('data', function (data) {
       body += data;
     });
-    request.on('end', function() {
+    request.on('end', function () {
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      });
     });
-    response.writeHead(200);
-    response.end('success');
   }
   else {
     response.writeHead(404);
